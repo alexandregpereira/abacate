a981254d475f2f5be1c1bd0c02ac270cd4a11534
Alexandre Pereira <alexandre.gomes2606@gmail.com>
2021-03-07 15:39:41 -0300

    Adjust colors

--- app/src/main/java/br/alexandregpereira/abacate/ui/theme/Color.kt
+++ app/src/main/java/br/alexandregpereira/abacate/ui/theme/Color.kt
@@ -2,9 +2,8 @@ package br.alexandregpereira.abacate.ui.theme
 
 import androidx.compose.ui.graphics.Color
 
-val Purple200 = Color(0xFFBB86FC)
-val Purple500 = Color(0xFF6200EE)
-val Purple700 = Color(0xFF3700B3)
-val Teal200 = Color(0xFF03DAC5)
+val Green200 = Color(0xFF8BC34A)
+val Green500 = Color(0xFF4CAF50)
+val Green700 = Color(0xFF397A3C)
 val White = Color(0xFFFFFFFF)
 val GrayScale100 = Color(0xFFE4E4E4)
