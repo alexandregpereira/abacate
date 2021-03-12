e86eb4a547a08e88188b8ef7b8a71216f5cadb14
Alexandre Pereira <alexandre.gomes2606@gmail.com>
2021-03-12 05:07:20 -0300

    Add circle animation

--- app/src/main/java/br/alexandregpereira/abacate/ui/component/BottomBar.kt
+++ app/src/main/java/br/alexandregpereira/abacate/ui/component/BottomBar.kt
@@ -1,56 +1,49 @@
 package br.alexandregpereira.abacate.ui.component
 
+import androidx.compose.animation.core.animateDpAsState
 import androidx.compose.animation.core.animateFloatAsState
 import androidx.compose.foundation.Canvas
 import androidx.compose.foundation.clickable
-import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.fillMaxHeight
-import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.height
-import androidx.compose.material.Icon
-import androidx.compose.material.MaterialTheme
+import androidx.compose.foundation.layout.*
 import androidx.compose.material.Surface
-import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.outlined.ArrowForward
 import androidx.compose.runtime.*
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.geometry.Offset
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.Path
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.Dp
 import androidx.compose.ui.unit.dp
 import br.alexandregpereira.abacate.ui.theme.AbacateTheme
 
 @Composable
 fun BottomBar() {
     val modifier = Modifier
-        .height(40.dp)
+        .height(56.dp)
         .fillMaxWidth()
 
-    val itemCount = 6
-    var animationData by remember { mutableStateOf(List(itemCount) { it == 0 }) }
-
-    val curveInterpolatorList = animationData.map {
-        animateFloatAsState(targetValue = if (it) 0.6f else 0f).value
+    val itemCount = 4
+    var itemIndex by remember { mutableStateOf(0f) }
+    val itemIndexState by animateFloatAsState(targetValue = itemIndex)
+    val radiusList = (0..itemCount).map { index ->
+        animateDpAsState(targetValue = if (index.toFloat() == itemIndex) 24.dp else 0.dp).value
     }
 
     DrawBottomBar(
         itemCount = itemCount,
-        curveInterpolatorList = curveInterpolatorList,
+        itemIndex = itemIndexState,
+        radiusList = radiusList,
         modifier
     )
 
     Row(modifier) {
         (0 until itemCount).forEach { index ->
-            Icon(
-                imageVector = Icons.Outlined.ArrowForward,
-                tint = MaterialTheme.colors.primary,
-                contentDescription = null,
+            Box(
                 modifier = Modifier
                     .weight(1f)
                     .fillMaxHeight()
                     .clickable {
-                        animationData = List(itemCount) { it == index }
+                        itemIndex = index.toFloat()
                     }
             )
         }
@@ -60,30 +53,40 @@ fun BottomBar() {
 @Composable
 private fun DrawBottomBar(
     itemCount: Int,
-    curveInterpolatorList: List<Float>,
+    itemIndex: Float,
+    radiusList: List<Dp>,
     modifier: Modifier = Modifier
 ) {
     Canvas(modifier = modifier,
         onDraw = {
-            val itemWidth = size.width / itemCount
-            val itemCenter = itemWidth / 2
-            val topY = 0f
             val bottomY = size.height
-            val curveCentersAndRadiusList = curveInterpolatorList.mapIndexed { index, percentage ->
-                (itemCenter + (itemWidth * index)) to bottomY * percentage
+            val itemWidth = size.width / itemCount
+            val marginTop = 8.dp.toPx()
+            val curveHeight = size.height * 0.78f + marginTop
+            val itemCenter = itemWidth / 2
+            val curveCenter = itemCenter + (itemWidth * itemIndex)
+
+            radiusList.forEachIndexed { index, radius ->
+                drawCircle(
+                    Color.Green,
+                    radius = radius.toPx(),
+                    center = Offset(
+                        itemCenter + (itemWidth * index),
+                        (curveHeight / 2) - 2.dp.toPx()
+                    )
+                )
             }
-//            Log.d(
-//                "Canvas",
-//                "index = $indexSelected($curveCenter, $curveCircleRadius) - lastIndexSelected = $lastIndexSelected($lastCurveCenter, $lastCurveCircleRadius)"
-//            )
             drawPath(
                 path = Path().apply {
-                    curveCentersAndRadiusList.forEach {
-                        drawCircle(
-                            it.first, it.second
-                        )
-                    }
-                    lineTo(size.width, topY)
+                    lineTo(0f, marginTop)
+                    drawCircle(
+                        marginTop = marginTop,
+                        width = size.width,
+                        curveHeight = curveHeight,
+                        curveCenter = curveCenter,
+                        convertToPx = { it.toPx() }
+                    )
+                    lineTo(size.width, marginTop)
                     lineTo(size.width, bottomY)
                     lineTo(0f, bottomY)
                     close()
@@ -94,47 +97,52 @@ private fun DrawBottomBar(
     )
 }
 
-private data class AnimationData(
-    val lastWithCurve: Boolean = false,
-    val withCurve: Boolean = true,
-    val lastIndexSelected: Int = 0,
-    val indexSelected: Int = 0
-)
+private const val MAX_ITEMS = 5
 
 private fun Path.drawCircle(
+    marginTop: Float,
+    width: Float,
+    curveHeight: Float,
     curveCenter: Float,
-    curveCircleRadius: Float
+    convertToPx: (Dp) -> Float
 ) {
-    val firstCurveStartPoint =
-        Offset(curveCenter - (curveCircleRadius * 2) - (curveCircleRadius / 3), 0f)
+    val minSize = width / MAX_ITEMS
+    val minSizeCenter = minSize / 2
 
-    val point3 =
-        Offset(curveCenter, curveCircleRadius + (curveCircleRadius / 4))
-
-    val point6 =
-        Offset(curveCenter + (curveCircleRadius * 2) + (curveCircleRadius / 3), 0f);
+    val firstCurveStartPoint = Offset(
+        curveCenter - minSizeCenter,
+        marginTop
+    )
 
     val point1 = Offset(
-        firstCurveStartPoint.x + curveCircleRadius + (curveCircleRadius / 4),
+        convertToPx(12.dp) + (curveCenter - minSizeCenter),
         firstCurveStartPoint.y
     )
 
     val point2 = Offset(
-        point3.x - (curveCircleRadius * 2) + curveCircleRadius,
-        point3.y
+        -convertToPx(8.dp) + (curveCenter - minSizeCenter),
+        curveHeight
     )
 
+    val point3 = Offset(curveCenter, curveHeight)
+
     val point4 = Offset(
-        point3.x + (curveCircleRadius * 2) - curveCircleRadius,
-        point3.y
+        curveCenter + (curveCenter - point2.x),
+        point2.y
     )
 
     val point5 = Offset(
-        point6.x - (curveCircleRadius + (curveCircleRadius / 4)),
-        point6.y
+        curveCenter + (curveCenter - point1.x),
+        point1.y
     )
 
-    lineTo(firstCurveStartPoint.x, firstCurveStartPoint.y);
+    val point6 = Offset(
+        curveCenter + (curveCenter - firstCurveStartPoint.x),
+        firstCurveStartPoint.y
+    )
+
+
+    lineTo(firstCurveStartPoint.x, firstCurveStartPoint.y)
     cubicTo(
         x1 = point1.x, y1 = point1.y,
         x2 = point2.x, y2 = point2.y,
